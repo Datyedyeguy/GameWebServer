435a3b2b30d7ff0a989a230bfc29e7fc5cf18037
Hans Brigman <hans@datyedyeguy.net>
2014-12-06 09:10:22 -0500

    Initial Commit.

---
+++ GameWebServer/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace GameWebServer
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
