64bd291c015632e111d70256d6603f103bae3517
Hans Brigman <hans@datyedyeguy.net>
2014-12-09 12:49:01 -0500

    Made the server able to run as a service.

--- GameWebServer/Program.cs
+++ GameWebServer/Program.cs
@@ -1,23 +1,17 @@
-﻿namespace GameWebServer
-{
-    using System;
-    using Nancy.Hosting.Self;
+﻿using System;
+using System.ServiceProcess;
 
+using Nancy.Hosting.Self;
+
+namespace GameWebServer
+{
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var uri =
-                new Uri("http://localhost:80");
+            var servicesToRun = new ServiceBase[] { new Service() };
 
-            using (var host = new NancyHost(uri))
-            {
-                host.Start();
-
-                Console.WriteLine("Your application is running on " + uri);
-                Console.WriteLine("Press any [Enter] to close the host.");
-                Console.ReadLine();
-            }
+            ServiceBase.Run(servicesToRun);
         }
     }
 }
---
+++ GameWebServer/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+
+using Nancy.Hosting.Self;
+
+namespace GameWebServer
+{
+    class Service : ServiceBase
+    {
+        private readonly NancyHost _nancyHost;
+
+        public Service() 
+        {
+            var uri = new Uri("http://localhost:80");
+            _nancyHost = new NancyHost(uri);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            _nancyHost.Start();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            _nancyHost.Stop();
+        }
+    }
+}
